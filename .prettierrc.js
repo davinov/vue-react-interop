3b0929cfe0da385b59f58d6d9a9c953419857240
David Nowinsky <david.nowinsky@toucantoco.com>
2024-02-16 14:53:18 +0100

    Formatting config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2
+};
+
+export default config;
